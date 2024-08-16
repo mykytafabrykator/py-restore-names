4d25e5b102d132172ef99fb7fd16c086ea4c2e9e
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2024-08-16 16:16:22 +0300

    Implement solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,35 @@
+from typing import List
+
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.mark.parametrize(
+    "value,expected_result",
+    [
+        (
+            [{"first_name": None,
+              "last_name": "Holy",
+              "full_name": "Jack Holy"}],
+            [{"first_name": "Jack",
+              "last_name": "Holy",
+              "full_name": "Jack Holy"}]
+        ),
+        (
+            [{"last_name": "Adams",
+             "full_name": "Mike Adams"}],
+            [{"first_name": "Mike",
+              "last_name": "Adams",
+              "full_name": "Mike Adams"}]
+        ),
+    ],
+    ids=[
+        "First name key set to None",
+        "No first name key"
+    ]
+)
+def test_cryptocurrency_action(value: List[dict],
+                               expected_result: List[dict]) -> None:
+    restore_names(value)
+    assert value == expected_result
